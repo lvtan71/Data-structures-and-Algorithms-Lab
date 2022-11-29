1f2643e386b44912cb189caf239aa88c7132a306
lvtan71 <levantan0701@gmail.com>
2022-11-30 03:01:20 +0700

    Complete sample print documents - tree

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 #include<string>
 #include<filesystem>
 #include<fstream>
+#include<vector>
 using namespace std;
 namespace fs = std::filesystem;
 
 struct Node {
 	string path;
-	vector<Node*> next;
+	vector<Node*> child;
 };
 
 Node* insert(Node* root, string path, int level) {
@@ -34,9 +35,59 @@ Node* createTrie(string path) {
 void printTrie(const string& prefix, const Node* node, bool isEnd) {
 	if (node != NULL) {
 		cout << prefix;
-		cout << (isEnd ? "|---" : "----");
+		cout << "|___";
 
 		cout << node->path << endl;
+		
+		for(int i = 0; i < node->child.size() ; i++){
+			if(i == node->child.size() - 1) printTrie(prefix + (isEnd ? "|    " : "     "), node->child[i], false);
+			else printTrie(prefix + (isEnd ? "|    " : "     "), node->child[i], true);
+		}
+		// Su dung DFS, de quy de tim duong dan, cay con trai sang phai
+		// Duyet cay khi den node la thi luu node la = key, value = duong dan tuyet doi -> luu vao map 
+	}
+}
+
+Node* newNode(string path){
+	Node* temp = new Node();
+	temp->path = path;
+	temp->child = {NULL};
+	return temp;
+}
+
+// TEST
+struct sNode{
+	string path;
+	sNode *left;
+	sNode *right;
+};
+
+sNode* insert(sNode* &root, string path, bool a){
+	sNode* temp = new sNode();
+	temp->path = path;
+	temp->left = NULL;
+	temp->right = NULL;
+	if(root == NULL) root = temp;
+	else if(a){
+		root->left = temp;
+	}
+	else{
+		root->right = temp;
+	}
+	return root;
+}
+
+
+void printBT(const string& prefix, const sNode* node, bool isLeft) {
+	int i = 0;
+	if (node != NULL) {
+		cout << prefix;
+		cout << (isLeft ? "|___ " : "|___ ");
+
+		cout << node->path << endl;
+
+		printBT(prefix + (isLeft ? "|    " : "     "), node->left, !isLeft);
+		printBT(prefix + (isLeft ? "|    " : "     "), node->right, false);
 	}
 }
 int main() {
@@ -68,6 +119,42 @@ int main() {
 	//	// Liet ke thu muc va cac file trong thu muc do
 	//}
 
+	//TEST
+	sNode *root = new sNode();
+	root = NULL;
+	root = insert(root, "D:\\",0);
+	root = insert(root, "F1",0);
+	root = insert(root, "F2",1);
+	root->left = insert(root->left,"F3",1);
+	root->left->left = insert(root->left->left,"file.txt",1);
+	root->right = insert(root->right,"null.txt",0);
+	root->right = insert(root->right,"notnull.txt",1);
+	printBT("",root,false);
 	
+	///
+
+	Node* nroot = new Node();
+	nroot->path = "D:\\";
+	nroot->child.push_back(newNode("F1"));
+	nroot->child.push_back(newNode("F2"));
+	nroot->child.push_back(newNode("F3"));
+	nroot->child.push_back(newNode("F4"));
+	nroot->child.push_back(newNode("F5"));
+	nroot->child[0]->child.push_back(newNode("F6"));
+	nroot->child[0]->child.push_back(newNode("F7"));
+	nroot->child[0]->child.push_back(newNode("F8"));
+	nroot->child[0]->child.push_back(newNode("F9"));
+	nroot->child[0]->child[1]->child.push_back(newNode("F10"));
+	nroot->child[0]->child[3]->child.push_back(newNode("F11"));
+	nroot->child[0]->child[4]->child.push_back(newNode("F12"));
+	nroot->child[1]->child.push_back(newNode("a.txt"));
+	nroot->child[1]->child.push_back(newNode("b.txt"));
+	nroot->child[3]->child.push_back(newNode("c.txt"));
+	nroot->child[4]->child.push_back(newNode("d.txt"));
+	nroot->child[4]->child.push_back(newNode("e.txt"));
+	nroot->child[4]->child.push_back(newNode("f.txt"));
+
+	printTrie("",nroot,false);
+
 	return 0;
 }
