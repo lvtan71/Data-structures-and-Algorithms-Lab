42edf55b8b62de491ded0e2dda5d6c5622c1a151
3n0ch <iamnmt03@gmail.com>
2022-12-01 15:47:22 +0700

    test remove method of linked list

--- q2/test/ll.cpp
+++ q2/test/ll.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 void test_add_and_retrieve() {
-  cout << "[Test] Add and retrieve node in linked list" << endl;
+  cout << "[Test] Add and retrieve node in linked list - ";
 
   LinkedList L;
   L.addHead("a");
@@ -16,10 +16,55 @@ void test_add_and_retrieve() {
   assert(L.retrieve("b", result));
   assert(!L.retrieve("d", result));
 
-  cout << "Passed!" << endl;
+  cout << "Passed" << endl;
+}
+
+void test_remove() {
+  LinkedList L;
+  L.addHead("a");
+  L.addHead("b");
+  L.addHead("c");
+  L.addHead("d");
+  FileData result;
+
+  cout << "[Test] Remove head in linked list - ";
+  assert(L.remove("a", result, false));
+  cout << "Passed" << endl;
+
+  cout << "[Test] Remove node not in linked list - ";
+  assert(!L.remove("a", result, false));
+  cout << "Passed" << endl;
+
+  cout << "[Test] Empty the list - ";
+  assert(L.remove("d", result, false));
+  assert(L.remove("c", result, false));
+  assert(L.remove("b", result, false));
+  assert(L.isEmpty());
+  cout << "Passed" << endl;
+}
+
+void test_integration_1() {
+  LinkedList L;
+  L.addHead("a");
+  L.addHead("b");
+
+  cout << "[Test] Retrieve node -> Add Data -> Remove Node - ";
+
+  FileData *p_result, result;
+
+  assert(L.retrieve("b", p_result));
+  p_result->abs_path.push_back("ok");
+
+  assert(L.remove("b", result, true));
+  assert(result.abs_path.size() == 1);
+  assert(result.abs_path[0] == "ok");
+
+  cout << "Passed";
 }
 
 int main() {
   test_add_and_retrieve();
+  test_remove();
+  test_integration_1();
   return 0;
 }
