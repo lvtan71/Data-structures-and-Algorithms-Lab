6258385079ac6a3b66353bbbc8965161ad9166a9
3n0ch <iamnmt03@gmail.com>
2022-12-02 18:00:57 +0700

    misc: add docstring to FileData and FolderData

--- q2/include/Data.h
+++ q2/include/Data.h
@@ -13,17 +13,45 @@ struct BaseData {
   virtual void append(const string &path) = 0;
 };
 
+/*
+ * Vector of paths
+ *
+ * Example: ["d:\a\xyz", "d:\b\xyz"]
+ */
 struct FileData : public BaseData {
   FileData() = default;
   ~FileData() = default;
+
+  /*
+   * @note assume path has utils::kFolderNFile type
+   * @onerror do not append path
+   */
   void append(const string &path);
 
   vector<string> abs_path;
 };
+
+/* 
+ * HashTable: (key, value) = (file, paths)
+ *
+ * Example:
+ *  xyz: ["d:\a\xyz", "d:\b\xyz"]
+ *  123: ["d:\a\123", "d:\b\123"]
+ */
 struct FolderData : public BaseData {
   FolderData() = default;
   FolderData(int size);
   ~FolderData();
   
+  /*
+   * @note assume path has utils::kFolderNFile type
+   * @onerror do not append path
+   *
+   * Example usage:
+   *  fileData.append("d:\abc\xy")
+   *  => abs_path->insertPath("xy", "d:\abc\xy")
+   */
+  void append(const string &path);
+
   HashTable<FileData> *abs_path;
 };
